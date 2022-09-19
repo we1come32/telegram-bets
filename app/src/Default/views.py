70217d817adc12e9c5149c4bbcd77a075a835f9f
Ilya Kanyshev <26299517+we1come32@users.noreply.github.com>
2022-09-19 08:44:27 +0300

    Moved static, configurated nginx.conf

--- app/src/Default/views.py
+++ app/src/Default/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
+
+
+def started_redirect(request):
+    # todo: redirect to name url
+    return HttpResponseRedirect(request, name="")
